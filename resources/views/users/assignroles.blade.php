ca6f82f7f6fb18ec23154b7c061cff3def1a59f2
Adacasey <159260686+Adacasey@users.noreply.github.com>
2026-04-30 00:34:00 +0100

    added permissions to roles

--- resources/views/users/assignroles.blade.php
+++ resources/views/users/assignroles.blade.php
@@ -7,19 +7,19 @@
 </style>
 <section class="content-header">
     <h1>
-        Roles for User: <b>{{$user->name}}</b>
+        Permissions for Role: <b>{{$role->name}}</b>
     </h1>
 </section>
 <div class="content">
     <div class="box box-primary">
         <div class="box-body">
-            {!! Form::model($user, ['route' => ['roles.rolesupdate', $user->id], 'method' => 'patch']) !!}
+            {!! Form::model($role, ['route' => ['roles.permissionsupdate', $role->id], 'method' => 'patch']) !!}
             <div class="row" style="padding-left: 20px">
                 <div class="form-group col-sm-4">
-                    @foreach($roles as $role)
-                        <label class="control-label col-sm-10">{{$role->name}}</label>
-                        <div class="col-sm-2"><input class="checkbox-inline" type="checkbox" name="role[{{$role->id}}]"
-                        @if($user->hasRole($role)) checked @endif /></div>
+                    @foreach($permissions as $permission)
+                        <label class="control-label col-sm-10">{{$permission->name}}</label>
+                        <div class="col-sm-2"><input class="checkbox-inline" type="checkbox" name="permission[{{$permission->id}}]"
+                        @if($role->getAllPermissions()->contains($permission)) checked @endif /></div>
                     @endforeach
                 </div>
                 <div class="form-group col-sm-12">
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/loggedInMember','App\Http\Controllers\memberController@getLoggedInM
 
 Route::resource('users', App\Http\Controllers\usersController::class);
 Route::resource('roles', App\Http\Controllers\rolesController::class);
+Route::resource('permissions', App\Http\Controllers\permissionsController::class);
 
 Route::get('/users/assignroles/{id}', 'App\Http\Controllers\usersController@assignRoles')->name('users.assignroles');
 Route::patch('/users/updateroles/{id}', 'App\Http\Controllers\usersController@updateRoles')->name('roles.rolesupdate');
+
+Route::get('/roles/assignpermissions/{id}', 'App\Http\Controllers\rolesController@assignPermissions')->name('roles.assignpermissions');
+Route::patch('/roles/updatepermissions/{id}', 'App\Http\Controllers\rolesController@updatePermissions')->name('roles.permissionsupdate');
 
 require __DIR__.'/auth.php';
