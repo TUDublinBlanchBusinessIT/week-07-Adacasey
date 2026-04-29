ff58e244bc37d618ec9a902188fb41d4216235f3
Adacasey <159260686+Adacasey@users.noreply.github.com>
2026-04-29 17:10:49 +0100

    added naAuth login link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tennis Club</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <title>Title TennisClub</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
 </head>
 <body>
-    @include('layouts.navigation')
-    <div class="container">
-        @yield('content')
+    <nav class="navbar navbar-default navbar-static-top">
+        <ul class="nav navbar-nav">
+            <li><a href="{{route('members.index')}}">Members</a></li>
+            <li><a href="{{route('members.create')}}">New Member</a></li>
+        </ul>
+        @include('layouts.navAuth')
+    </nav>
+    <div id="page-content-wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                <div class="col-lg-8"> @yield('content') </div>
+                <div class="col-lg-2"></div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
