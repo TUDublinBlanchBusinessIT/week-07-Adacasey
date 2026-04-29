465c78bd79007410b25dd71ca15b3bda075ba888
Adacasey <159260686+Adacasey@users.noreply.github.com>
2026-04-29 17:02:49 +0100

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tennis Club</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    @include('layouts.navigation')
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
