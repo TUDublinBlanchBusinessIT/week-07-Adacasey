105df25c36bcf62e56044b1912ef8c377e4a5e8c
Adacasey <159260686+Adacasey@users.noreply.github.com>
2026-04-30 01:03:27 +0100

    added admin menu and enforced permissions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <title>Title TennisClub</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
     <nav class="navbar navbar-default navbar-static-top">
@@ -12,7 +14,17 @@
             <li><a href="{{route('members.index')}}">Members</a></li>
             <li><a href="{{route('members.create')}}">New Member</a></li>
         </ul>
-        @include('layouts.navAuth')
+        <ul class="nav navbar-nav navbar-right" style="margin-right:10px">
+            @if(Auth::check())
+                @if(Auth::user()->hasRole('System Admin'))
+                    @include('layouts.adminmenu')
+                @endif
+                <li><a href="{!! route('logout') !!}"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            @else
+                <li><a href="{!! route('login') !!}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li><a href="{!! route('register') !!}"><span class="glyphicon glyphicon-user"></span> Register</a></li>
+            @endif
+        </ul>
     </nav>
     <div id="page-content-wrapper">
         <div class="container-fluid">
